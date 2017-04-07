3400414c9a55441f1064699aace12ce10df40ef1
Lucas Vieira <vieiralucas4@gmail.com>
2017-04-07 11:26:33 -0300

    Add missing semicolon

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import { applyMiddleware, createStore } from 'redux'
-import logger from 'redux-logger'
-import rootReducer from './reducer'
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from './reducer';
 
 export const configureStore = preloadedState => createStore(
   rootReducer,
   preloadedState,
   applyMiddleware(logger)
-)
+);
